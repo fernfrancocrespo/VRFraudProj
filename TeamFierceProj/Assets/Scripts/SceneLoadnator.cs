075fd0268fbf5ba5dc75f2204e61af31850c9508
Fernando Crespo <fernfrancocrespo@gmail.com>
2018-11-28 13:57:16 -0500

    big one

---
+++ TeamFierceProj/Assets/Scripts/SceneLoadnator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadnator : MonoBehaviour {
+    public string sceneToLoad;
+
+	// Use this for initialization
+	public void SceneLoad () {
+        SceneManager.LoadScene(sceneToLoad);
+		
+	}
+
+}
