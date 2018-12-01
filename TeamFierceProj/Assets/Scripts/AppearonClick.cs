2ac02e13e522cb0e60ac792d91acc54d5eee92be
mimmam1464 <44204526+mimmam1464@users.noreply.github.com>
2018-12-01 17:59:50 -0500

    Added subtitle and Scene 2
    **MESSAGE IS PARENT OBJECT
    Have appear after as a new object
    Have disappear in the text itsefl that will be in canvas

---
+++ TeamFierceProj/Assets/Scripts/AppearonClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearonClick : MonoBehaviour {
+    // The inspector changable variables
+    public GameObject text;
+    public float DestroyAfter;
+
+    // Use this for initialization
+
+
+    private bool appearNow=false;
+    bool timer = false;
+
+
+    public void Start()
+    {
+        text.SetActive(false);
+        
+    }
+
+    public void activeNow()
+    {
+        appearNow = true;
+        timer = true;
+    }
+
+     void Update()
+    {
+      
+        if (appearNow == true)
+        {
+            text.SetActive(true);
+        }
+        if (DestroyAfter > 0 & timer == true)
+            DestroyAfter = DestroyAfter - Time.deltaTime;
+
+        if (DestroyAfter < 0 & timer == true)
+            text.SetActive(false);
+      
+    }
+	}
+
---
+++ TeamFierceProj/Assets/Scripts/Disappear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear: MonoBehaviour {
+   
+    public GameObject text;
+
+    // Use this for initialization
+    private void Start()
+    {   
+    }
+   
+   
+
+    // Update is called once per frame
+    void Update () {
+       
+           
+            text.SetActive(false);
+          
+      
+
+     
+        
+		
+	}
+}
