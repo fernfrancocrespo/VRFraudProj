1e6a98c56e46ed3a138bf890b9556a10e01193f5
mimmam1464 <44204526+mimmam1464@users.noreply.github.com>
2018-12-01 20:08:11 -0500

    Scene 2 is complete
    Sprint 2: Final for the Sprint.

---
+++ TeamFierceProj/Assets/Scripts/TextAppear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAppear : MonoBehaviour
+{
+
+    public float ActivateAfter;
+    public GameObject text;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ActivateAfter > 0)
+        {
+
+            text.SetActive(false);
+            ActivateAfter -= Time.deltaTime;
+        }
+        else if (ActivateAfter <= 0)
+        {
+
+            text.SetActive(true);
+        }
+    }
+}
+
