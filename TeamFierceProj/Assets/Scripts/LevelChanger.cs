a59a8516064787273c9109b96517ff2dff2a20a7
kyuseo park <kyuseopark@kyuseoui-MacBook-Pro.local>
2018-12-03 12:38:39 -0500

    transition

---
+++ TeamFierceProj/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelChanger : singleton<LevelChanger>
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    protected LevelChanger() { }
+
+    // Update is called once per frame
+ //   void Update () {
+
+	//	 if (Input.GetMouseButtonDown(0))
+ //        {
+ //            FadeToLevel(1);
+ //        }
+		
+	//}
+
+	public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete ()
+    {
+        animator.ResetTrigger("FadeOut");
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
--- TeamFierceProj/Assets/Scripts/Timer.cs
+++ TeamFierceProj/Assets/Scripts/Timer.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 
 public class Timer : MonoBehaviour
 {
-    public string levelToLoad;
+    //public string levelToLoad;
     public float timer = 10f;
     private Text timerSecond;
+    //public int currentScene;
+    public int nextScene;
 
     // Use this for initialization
     void Start()
     {
+
         timerSecond = GetComponent<Text>();
 
     }
@@ -23,8 +26,31 @@ public class Timer : MonoBehaviour
         timerSecond.text = timer.ToString("f2");
         if (timer <= 0)
         {
-            Application.LoadLevel(levelToLoad);
+            //Application.LoadLevel(levelToLoad);
+            // FadeToLevel(0);
+
+            LevelChanger.Instance.FadeToLevel(nextScene);
+
+            //switch (sceneNum)
+            //{
+            //    case 0:
+
+            //        break;
+            //    case 1:
+            //        LevelChanger.Instance.FadeToLevel(0);
+            //        break;
+            //    case 2:
+            //        LevelChanger.Instance.FadeToLevel(1);
+            //        break;
+            //    default:
+
+            //        break;
+            //}
+
+
 
         }
+        
     }
+    
 }
