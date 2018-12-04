bcdd5d4b674ab2b5161dadab0ac55f09e3537c72
mimmam1464 <44204526+mimmam1464@users.noreply.github.com>
2018-12-03 23:26:55 -0500

    Scene 1 is now working at its best

---
+++ TeamFierceProj/Assets/Scripts/PlayAudioAfter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioAfter : MonoBehaviour {
+
+    AudioSource myAudio;
+    public float timer;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(timer);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
