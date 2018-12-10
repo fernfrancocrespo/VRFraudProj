7bbaed1c5b32c6ddd23f9b85992c1154fff466c0
mimmam1464 <44204526+mimmam1464@users.noreply.github.com>
2018-12-09 19:12:41 -0500

    Scene 24

---
+++ TeamFierceProj/Assets/Scripts/ButtonAfterClickTime.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonAfterClickTime : MonoBehaviour
+{
+    //public string levelToLoad;
+    public float timer = 10f;
+    //public int currentScene;
+    public GameObject Object;
+    private bool activate=false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (activate==false) Object.SetActive(false);
+
+    }
+
+    public void startTimer()
+    {
+        activate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (activate==true) timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            //Application.LoadLevel(levelToLoad);
+            // FadeToLevel(0);
+
+            Object.SetActive(true);
+
+            //switch (sceneNum)
+            //{
+            //    case 0:
+
+            //        break;
+            //    case 1:
+            //        LevelChanger.Instance.FadeToLevel(0);
+            //        break;
+            //    case 2:
+            //        LevelChanger.Instance.FadeToLevel(1);
+            //        break;
+            //    default:
+
+            //        break;
+            //}
+
+
+
+        }
+        
+    }
+    
+}
--- TeamFierceProj/Assets/Scripts/Disappear.cs
+++ TeamFierceProj/Assets/Scripts/Disappear.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Disappear: MonoBehaviour {
    
-    public GameObject text;
+    public GameObject Object;
+    private bool disappear = false;
 
     // Use this for initialization
     private void Start()
     {   
     }
-   
+
+    public void disNow()
+    {
+        disappear = true;
+    }
    
 
     // Update is called once per frame
     void Update () {
        
-           
-            text.SetActive(false);
+           if (disappear==true)
+           Object.SetActive(false);
           
       
 
