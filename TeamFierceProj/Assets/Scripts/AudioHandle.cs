5242d2502bacf41963ef56c8d0fcfb5311134670
kyuseo park <kyuseopark@kyuseoui-MacBook-Pro.local>
2018-12-10 09:55:14 -0500

    Talking on the phone motion

---
+++ TeamFierceProj/Assets/Scripts/AudioHandle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandle : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float startAfter = 2f;
+
+    // Use this for initialization
+    void Start () {
+        audioSource.clip = clip;
+        audioSource.Pause();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startAfter -= Time.deltaTime;
+        if (startAfter >= 0.3f && startAfter <= 0.5f)
+        {
+            audioSource.Play(0);
+        }
+    }
+}
